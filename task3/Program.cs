43845dc26e3101b19515b440d163262171b737ba
JarLaX99 <vanek10699@gmail.com>
2023-06-18 03:22:50 +0300

    семинар 3, задача 3

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= numberN)
+{
+    Console.Write(Math.Pow(count, 3) + " ");
+    count++;
+}
