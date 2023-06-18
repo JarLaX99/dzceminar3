b9935bb32478551b9123837f96eed0c48f2e1a49
JarLaX99 <vanek10699@gmail.com>
2023-06-18 03:06:12 +0300

    семинар 3, задача 1

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.Write("Введите 5-ти значное число: ");
+string str = Console.ReadLine()!;
+
+if (str.Length == 5)
+{
+    if (str[0] == str[4] && str[1] == str[3])
+    {
+        System.Console.WriteLine($"число {str} является палиндромом ");
+    }
+    else
+    {
+        System.Console.WriteLine($"число {str} не является палиндромом ");
+    }
+}
+else
+{
+    System.Console.WriteLine(" Ошибка:Введите 5-ти значное число");
+}
